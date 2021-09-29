e9ed4b2c4efd5cad724640bf9b3a06a99afba26a
RobertDober <robert.dober@gmail.com>
2021-09-29 20:05:32 +0200

    README

--- lib/tag_cloud.ex
+++ lib/tag_cloud.ex
@@ -1,5 +1,12 @@
 defmodule TagCloud do
   @moduledoc """
+  
+  [![CI](https://github.com/RobertDober/tag_cloud/actions/workflows/ci.yml/badge.svg)](https://github.com/RobertDober/tag_cloud/actions/workflows/ci.yml)
+  [![Coverage Status](https://coveralls.io/repos/github/RobertDober/tag_cloud/badge.svg?branch=master)](https://coveralls.io/github/RobertDober/tag_cloud?branch=master)
+  [![Hex.pm](https://img.shields.io/hexpm/v/tag_cloud.svg)](https://hex.pm/packages/tag_cloud)
+  [![Hex.pm](https://img.shields.io/hexpm/dw/tag_cloud.svg)](https://hex.pm/packages/tag_cloud)
+  [![Hex.pm](https://img.shields.io/hexpm/dt/tag_cloud.svg)](https://hex.pm/packages/tag_cloud)
+
   Elxir Tools to create Tag clouds - Integration with Earmark Annotations - Independent Library Functions - Gamma correction for grey scales
   """
 
@@ -7,6 +14,7 @@ defmodule TagCloud do
   @doc """
   A convenience method to access this library's version
   """
+  @spec version :: binary()
   def version do
     :application.ensure_started(:tag_cloud)
     with {:ok, version} = :application.get_key(:tag_cloud, :vsn), do: to_string(version)
--- mix.exs
+++ mix.exs
@@ -1,4 +1,4 @@
-defmodule Extractly.MixProject do
+defmodule TagCloud.MixProject do
   use Mix.Project
 
   @version "0.1.0"
